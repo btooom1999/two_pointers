436197aceb4f6317bfcafa96366ac8e20f6fb9c4
btooom1999 <btooom1999@gmail.com>
2026-01-08 20:21:54 +0700

    finished merge_sorted_array exercise

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ mod valid_palindrome_2;
 mod valid_palindrome_4;
 mod valid_word_abbreviation;
 mod merge_strings_alternately;
+mod merge_sorted_array;
 
 fn main() {
-    merge_strings_alternately::main();
+    merge_sorted_array::main();
 }
